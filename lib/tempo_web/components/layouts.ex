08803a42ab32995c40dede40f03470bd3af15de9
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2026-01-02 15:26:01 -0500

    Remove unwanted margin

--- lib/tempo_web/components/layouts.ex
+++ lib/tempo_web/components/layouts.ex
@@ -35,7 +35,7 @@ defmodule TempoWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <header class="navbar px-4 sm:px-6 lg:px-8">
+    <header class="navbar px-4">
       <div class="flex-1">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
           <img src={~p"/images/logo.svg"} width="36" />
@@ -51,8 +51,8 @@ defmodule TempoWeb.Layouts do
       </div>
     </header>
 
-    <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl space-y-4">
+    <main class="px-4">
+      <div class="max-w-2xl space-y-4">
         {render_slot(@inner_block)}
       </div>
     </main>
